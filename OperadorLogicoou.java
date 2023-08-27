ec35783f0a76a29a99fce472715a81a49b172bb7
DevVictorPereiraAngelino <49197056+angelino-med-style@users.noreply.github.com>
2023-08-27 15:32:19 -0300

    add

---
+++ OperadorLogicoou.java
@@ -0,0 +1,16 @@
+package pacote2;
+
+public class OperadorLogicoou {
+
+	public static void main(String[] args) {
+		boolean a = true;
+		boolean b = false;
+		boolean c = false;
+		
+		System.out.println(a || b);
+		System.out.println(b || c);
+
+
+	}
+
+}
